dd7cf6343ccbc79143591bc597c12652a30be9ea
Hoffmann Julien <29329710+luminahateo@users.noreply.github.com>
2025-12-29 18:12:32 +0100

    Delete Systeme directory

--- Systeme/bios.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-clear
-echo "Mise à jour du BIOS
-======================================================================================="
-sudo fwupdmgr refresh --force 
-sudo fwupdmgr get-updates 
-sudo fwupdmgr update
--- Systeme/hostname.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-clear
-echo "Mise à jour du nom de système
-======================================================================================="
-hostnamectl set-hostname "System 76 Meerkat"
-hostnamectl status
